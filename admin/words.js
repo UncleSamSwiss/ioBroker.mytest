e6bef1ea1c505b5f23b8da04e060c51e2e076cf1
ioBroker Adapter Creator <noreply@iobroker.net>
2021-04-21 22:18:06 +0200

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,53 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"mytest adapter settings": {
+		"en": "Adapter settings for mytest",
+		"de": "Adaptereinstellungen für mytest",
+		"ru": "Настройки адаптера для mytest",
+		"pt": "Configurações do adaptador para mytest",
+		"nl": "Adapterinstellingen voor mytest",
+		"fr": "Paramètres d'adaptateur pour mytest",
+		"it": "Impostazioni dell'adattatore per mytest",
+		"es": "Ajustes del adaptador para mytest",
+		"pl": "Ustawienia adaptera dla mytest",
+		"zh-cn": "mytest的适配器设置"
+	},
+	"option1": {
+		"en": "Option 1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "Option 2",
+		"de": "Option 2",
+		"ru": "Вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	},
+	"option3": {
+		"en": "Option 3",
+		"de": "Option 3",
+		"ru": "Вариант 3",
+		"pt": "Opção 3",
+		"nl": "Optie 3",
+		"fr": "Option 3",
+		"it": "Opzione 3",
+		"es": "Opcion 3",
+		"pl": "Wariant 3",
+		"zh-cn": "选项3"
+	}
+};
